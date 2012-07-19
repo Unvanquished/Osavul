Unvanquished/Osavul

commit ad04aab5eefd67689ef57af92b3e9d9421465ffe

removed debug output; fixed white-colored output in the IRC tab.

mainwindow.cpp
@@ -285,7 +285,6 @@ void MainWindow::on_ircChat_serverCommMessage(const QString &s)
     static const QString templ = "[HH:mm:ss] ";
     QTime time = QTime::currentTime();
 
-    ui->ircDisplayArea->setTextColor(Qt::white);
     ui->ircDisplayArea->append(time.toString(templ) + s);
 }
 
@@ -330,7 +329,6 @@ void MainWindow::connectTo(const QString &host)
 
     ui->statusBar->showMessage(tr("Launching Unvanquished..."), 3000);
 
-    qDebug() << path << "+connect" << host;
     QProcess::startDetached(path, { "+connect", host });
 }
 

unv.cpp
@@ -108,9 +108,6 @@ void Server::receiveData()
 
 void Server::query()
 {
-    if (sock.write(m_queryMessage) == -1)
-        qDebug() << sock.error() << sock.errorString() << '!';
-
     m_ping = 0;
     pingTimer.start();
 }

ircclient.cpp
@@ -64,7 +64,6 @@ void IrcClient::init(const QString &nickName)
 void IrcClient::send(const QString &data)
 {
     sock.write(QString(data % "\r\n").toUtf8());
-    qDebug() << "<<" << data;
 }
 
 void IrcClient::say(Channel *channel, const QString &data)
@@ -187,8 +186,6 @@ void IrcClient::receive()
 
         // no CRLF, please
         QByteArray in = rawLine.left(rawLine.length() - 2);
-
-        qDebug() << in;
 
         // no prefix ':' either
         if (in.startsWith(':'))

osavul.pro.user
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE QtCreatorProject>
-<!-- Written by Qt Creator 2.5.0, 2012-07-20T00:29:14. -->
+<!-- Written by Qt Creator 2.5.0, 2012-07-20T01:10:38. -->
 <qtcreator>
  <data>
   <variable>ProjectExplorer.Project.ActiveTarget</variable>

commit f7a1e283b8f5adff787241cc2f1a1cf27980aa5e

fixed a silly regression from ad04aab, minor cleanup & refactoring

mainwindow.cpp
@@ -32,6 +32,10 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     chat = new IrcClient(this);
     chat->setObjectName("ircChat");
 
+    msv = new MasterServer();
+    msv->setParent(this);
+    msv->setObjectName("masterServer");
+
     // and this is where connectSlotsByName happens
     ui->setupUi(this);
 
@@ -67,14 +71,13 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     ui->humanTable->setItemDelegateForColumn(2, playerTableDelegate);
 
     QThread *unvThread = new QThread(this);
+    msv->setParent(nullptr);
     connect(qApp, SIGNAL(aboutToQuit()),
             unvThread, SLOT(quit()));
-    connect(&msv, SIGNAL(unvQueried(unv::GameServer *)),
-            this, SLOT(handle_unvanquished_queried(unv::GameServer *)));
     connect(unvThread, SIGNAL(started()),
-            &msv, SLOT(query()));
+            msv, SLOT(query()));
 
-    msv.moveToThread(unvThread);
+    msv->moveToThread(unvThread);
     unvThread->start();
 
     QThread *ircThread = new QThread(this);
@@ -141,7 +144,7 @@ void MainWindow::closeEvent(QCloseEvent *event)
     if (!trayIcon->isVisible())
         return;
 
-    bool doShow = settings.value("ui/showTrayNotice").toBool();
+    bool doShow = settings.value("ui/showTrayNotice", true).toBool();
 
     if (doShow) {
         QMessageBox mb(QMessageBox::Information,
@@ -195,7 +198,7 @@ void MainWindow::on_ircConnectButton_clicked()
     }
 }
 
-void MainWindow::handle_unvanquished_queried(unv::GameServer *sv)
+void MainWindow::on_masterServer_serverQueried(unv::GameServer *sv)
 {
     using namespace unv;
     static const QString templ = tr("%1 servers queried");
@@ -218,7 +221,7 @@ void MainWindow::handle_unvanquished_queried(unv::GameServer *sv)
     items.at(0)->setText(sv->game());
     items.at(1)->setText(sv->name());
     items.at(2)->setText(sv->map());
-    items.at(3)->setText(QString::number(sv->ping()));
+    items.at(3)->setData(Qt::EditRole, sv->ping());
     items.at(4)->setText(sv->formattedClientCount());
 
     if (isNew) {
@@ -294,7 +297,7 @@ void MainWindow::on_refreshButton_clicked()
     ui->serverTable->setRowCount(0);
 
     ui->refreshButton->setEnabled(false);
-    msv.query();
+    msv->query();
 
     // I am _really_ looking forward to C++11 lambda support in Qt; this is an ugly tmp workaround
     QTimer::singleShot(2000, this, SLOT(enableRefreshButton()));
@@ -324,7 +327,6 @@ void MainWindow::on_joinButton_clicked()
 
 void MainWindow::connectTo(const QString &host)
 {
-//    QProcess *proc = new QProcess(this);
     QString path = settings.value("unv/clientExecutablePath", "unvanquished").toString();
 
     ui->statusBar->showMessage(tr("Launching Unvanquished..."), 3000);
@@ -404,7 +406,7 @@ void MainWindow::on_playerFilterLineEdit_textEdited(const QString &arg1)
     if ((currentLength == 1 && previousLength == 0) // entering first letter
         || (currentLength < previousLength)) { // initial collection or generalization
         ui->playerTreeWidget->clear();
-        for (const GameServer *sv : msv.servers()) {
+        for (const GameServer *sv : msv->servers()) {
 
             if (sv->players().isEmpty())
                 continue;
@@ -530,14 +532,6 @@ void MainWindow::on_ircTabWidget_tabCloseRequested(int index)
 {
     if (index == 0 && chat->isConnected()) {
         ui->ircConnectButton->click();
-        return;
-    }
-
-    if (index == 0) {
-        QMessageBox::information(this,
-                                 "...",
-                                 "Excuse me wtf are you doing",
-                                 QMessageBox::Ok);
         return;
     }
 

mainwindow.h
@@ -82,18 +82,16 @@ private slots:
     void on_actionAbout_Osavul_triggered();
     void on_actionAbout_Qt_triggered();
     void connectTo(const QString &host);
-
     void on_actionPreferences_triggered();
 
 public slots:
     void on_ircChat_serverCommMessage(const QString &s);
-    void handle_unvanquished_queried(unv::GameServer *svNew);
+    void on_masterServer_serverQueried(unv::GameServer *svNew);
 
 private:
     void updateTeamTables(const QList<unv::Player> &playerList);
 
 signals:
-    void startIrcThread();
     void ircChangeNick(const QString &nick);
     void ircOpenQuery(const QString &user);
 
@@ -102,15 +100,13 @@ private:
     QSettings settings;
 
     Ui::MainWindow *ui;
-//    QThread *unvThread;
-//    QThread *ircThread;
     QProcess *gameProc;
     QHash<unv::GameServer *, QTableWidgetItem *> gameServersShown;
 
     QMenu *trayIconMenu;
     QSystemTrayIcon *trayIcon;
 
-    unv::MasterServer msv;
+    unv::MasterServer *msv;
     IrcClient *chat;
 };
 

settingsdialog.cpp
@@ -7,6 +7,7 @@ SettingsDialog::SettingsDialog(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->ircConnectOnStartupCheckBox->setChecked(settings.value("ircConnectOnStartup").toBool());
+    ui->daemonPathLineEdit->setText(settings.value("unv/clientExecutablePath").toString());
 }
 
 SettingsDialog::~SettingsDialog()
@@ -16,5 +17,6 @@ SettingsDialog::~SettingsDialog()
 
 void SettingsDialog::on_buttonBox_accepted()
 {
-    settings.setValue("ircConnectOnStartup", ui->ircConnectOnStartupCheckBox->isChecked());
+    settings.setValue("irc/connectOnStartup", ui->ircConnectOnStartupCheckBox->isChecked());
+    settings.setValue("unv/clientExecutablePath", ui->daemonPathLineEdit->text());
 }

settingsdialog.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>185</width>
-    <height>58</height>
+    <width>219</width>
+    <height>86</height>
    </rect>
   </property>
   <property name="windowTitle">
@@ -20,6 +20,23 @@
       <string>Connect to IRC on startup</string>
      </property>
     </widget>
+   </item>
+   <item>
+    <layout class="QHBoxLayout" name="daemonPathLayout">
+     <item>
+      <widget class="QLabel" name="daemonPathLabel">
+       <property name="text">
+        <string>&amp;Path to Daemon:</string>
+       </property>
+       <property name="buddy">
+        <cstring>daemonPathLineEdit</cstring>
+       </property>
+      </widget>
+     </item>
+     <item>
+      <widget class="QLineEdit" name="daemonPathLineEdit"/>
+     </item>
+    </layout>
    </item>
    <item>
     <widget class="QDialogButtonBox" name="buttonBox">

unv.cpp
@@ -108,6 +108,8 @@ void Server::receiveData()
 
 void Server::query()
 {
+    sock.write(m_queryMessage);
+
     m_ping = 0;
     pingTimer.start();
 }
@@ -219,5 +221,5 @@ void MasterServer::onGameSvReady() {
     GameServer *sv = qobject_cast<GameServer *> (sender());
     if (!sv) { Q_ASSERT(false); }
 
-    emit unvQueried(sv);
+    emit serverQueried(sv);
 }

unv.h
@@ -148,7 +148,7 @@ namespace unv {
         void processOOB(QList<QByteArray> st);
 
     signals:
-        void unvQueried(unv::GameServer *sv);
+        void serverQueried(unv::GameServer *sv);
 
     public slots:
         void onGameSvReady();